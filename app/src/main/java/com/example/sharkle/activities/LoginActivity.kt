62f8cb3c13dfade3fe719e2665970e5963a75561
eielelle <eielelle915@gmail.com>
2025-07-19 18:01:33 +0800

    updated theme

--- app/src/main/java/com/example/sharkle/activities/LoginActivity.kt
+++ app/src/main/java/com/example/sharkle/activities/LoginActivity.kt
@@ -8,20 +8,32 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.sharkle.components.Button
 import com.example.sharkle.components.OutlinedTextField
 
 @Composable
 fun LoginActivity(modifier: Modifier = Modifier) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     Surface(color = MaterialTheme.colorScheme.background, modifier = modifier.fillMaxSize()) {
-        Column( verticalArrangement = Arrangement.spacedBy(4.dp), modifier = modifier.padding(20.dp) ) {
-            Text(text = "Welcome Back!")
-            Text(text = "Email")
-            OutlinedTextField()
-            Text(text = "Password")
-            OutlinedTextField()
+        Column( verticalArrangement = Arrangement.spacedBy(12.dp), modifier = modifier.padding(20.dp) ) {
+            Text(text = "Welcome Back!", fontSize = 24.sp)
+            Column {
+                Text(text = "Email")
+                OutlinedTextField(value = email, onValueChange = { email = it })
+            }
+            Column {
+                Text(text = "Password")
+                OutlinedTextField(value = password, onValueChange = { password = it })
+            }
             Button()
         }
     }
--- app/src/main/java/com/example/sharkle/components/OutlinedTextField.kt
+++ app/src/main/java/com/example/sharkle/components/OutlinedTextField.kt
@@ -1,15 +1,20 @@
 package com.example.sharkle.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun OutlinedTextField(modifier: Modifier = Modifier) {
+fun OutlinedTextField(modifier: Modifier = Modifier, onValueChange: (String) -> Unit, value: String) {
     OutlinedTextField(
-        value = "",
-        onValueChange = {},
-        modifier = modifier.fillMaxWidth()
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier.fillMaxWidth(),
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = MaterialTheme.colorScheme.tertiary
+        )
     )
 }
